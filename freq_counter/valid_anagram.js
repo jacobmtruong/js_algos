87cd8538e92d1026a65f0c3e12f8e95bfef260f9
brandontaylor156 <brandon.david4@outlook.com>
2022-09-29 15:42:20 -0700

    prompt

--- freq_counter/valid_anagram.js
+++ freq_counter/valid_anagram.js
@@ -1,3 +1,5 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
 function validAnagram(str1, str2) {
     if (str1.length != str2.length){
         return false;
